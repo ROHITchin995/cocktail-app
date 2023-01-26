347247d46bdf62081fa8906271795c99cdb78e33
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-26 12:39:32 +0530

    context completed

--- src/context.js
+++ src/context.js
@@ -8,18 +8,40 @@ const AppProvider = ({ children }) => {
     const [searchTerm, setSearchTerm] = useState('a')
     const [cocktails, setCocktails] = useState([])
 
-    const fetchDrinks = useCallback(async ()=>{
+    const fetchDrinks = useCallback(async () => {
         setLoading(true)
-        try{
+        try {
             const response = await fetch(`${url}${searchTerm}`)
             const data = await response.json()
             console.log(data);
+            const { drinks } = data
+            if (drinks) {
+                const newCocktails = drinks.map((item) => {
+                    const { idDrink,
+                        strDrink,
+                        strDrinkThumb,
+                        strAlcoholic,
+                        strGlass, } = item
+
+                    return {
+                        id: idDrink,
+                        name: strDrink,
+                        image: strDrinkThumb,
+                        info: strAlcoholic,
+                        glass: strGlass
+                    }
+                })
+                setCocktails(newCocktails)
+            }else{
+                setCocktails([])
+            }
+            setLoading(false)
         }
-        catch(error){
+        catch (error) {
             console.log(error);
             setLoading(false)
         }
-    })
+    }, [searchTerm])
 
 
 
