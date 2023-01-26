ab5343daaff4f57a92f354d25c0ca1f43fce1bdc
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-26 13:56:05 +0530

    Cocktail list CSS added

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export const About =()=> {
   return (
     <section className="section about-section">
-      <h1 className="section-title">about us</h1>
+      <h1 className="section-title">About us</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
         repudiandae architecto qui adipisci in officiis, aperiam sequi atque
